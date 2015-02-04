9e183937cbead1732324a81cc157c2f4229fdc2d
adrianp23 <adrianpaiva@gmail.com>
2015-02-03 20:42:34 -0500

    fixed route error

--- app/routes.php
+++ app/routes.php
@@ -14,7 +14,9 @@
 
 // route to controller and function at specified URL
 
+Route::get('/', 'HomeController@showWelcome');
 Route::get('home', 'HomeController@showWelcome');
+
 Route::get('projects', 'ProjectController@showAllProjects');
 
 Route::get('users', 'UserController@showAllUsers');
