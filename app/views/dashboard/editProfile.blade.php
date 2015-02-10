d7a329751ecd7dee8e707201baa395520cc52499
adrianp23 <adrianpaiva@gmail.com>
2015-02-09 23:03:22 -0500

    no message

--- app/views/dashboard/editProfile.blade.php
+++ app/views/dashboard/editProfile.blade.php
@@ -4,8 +4,17 @@
 
 <div class="row">
     @include('dashboard/navPartial')
-    <h2>edit profile</h2>
 
+    <div class="col-xs-10">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h3 class="panel-title text-center">Edit Profile</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
+    </div>
 </div>
 
 @stop
